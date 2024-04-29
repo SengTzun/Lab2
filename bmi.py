717aa12f6d1d487457f1052125017b4f2719bd0f
Sengtzun <ysengt33.23@ichat.sp.edu.sg>
2024-04-29 15:50:59 +0800

    changes

--- bmi.py
+++ bmi.py
@@ -3,13 +3,14 @@ def calculate_bmi(height, weight):
     print("Weight="+ str(weight))
     bmi=weight/(height*height)
     print("bmi="+ str(bmi))
-    if (bmi<18.5):
-        print("Unnder Weight")
-        return -1
-    elif (18.5<=bmi<=25.0):
-        print("Normal weight")
-        return 0
-    elif (bmi>25.0):
-        print("Over Weight")
-        return 1
-calculate_bmi(weight=57, height=1.73)
+    calculate_bmi(weight=57, height=1.73)
+def checkbmi(bmi):
+     if (bmi<18.5):
+         print("Under Weight")
+         return -1
+     elif (18.5<=bmi<=25.0):
+           print("Normal weight")
+           return 0
+     elif (bmi>25.0):
+         print("Over Weight")
+         return 1
