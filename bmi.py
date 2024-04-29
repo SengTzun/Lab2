6eb4e90c524d7509e8bd2b950135deaaed960a84
Sengtzun <ysengt33.23@ichat.sp.edu.sg>
2024-04-29 15:15:27 +0800

    return value added

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(height, weight):
     print("bmi="+ str(bmi))
     if (bmi<18.5):
         print("Unnder Weight")
+        return -1
     elif (18.5<=bmi<=25.0):
         print("Normal weight")
+        return 0
     elif (bmi>25.0):
         print("Over Weight")
+        return 1
 calculate_bmi(weight=57, height=1.73)
